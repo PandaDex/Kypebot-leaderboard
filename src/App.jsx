b1e6bd9303e859fb190795778832aab493cc8575
Kacper Wochniak <78349074+PandaDex@users.noreply.github.com>
2023-03-30 15:33:15 +0200

    1.0.0-alpha

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [leaderboardData, setLeaderboardData] = useState([]);
 
   useEffect(() => {
-    fetch('http://3.121.207.210:3000/leaderboard-data')
+    fetch('https://aws.pandadex.ml/leaderboard-data')
       .then(response => response.json())
       .then(data => setLeaderboardData(data))
       .catch(error => console.error(error));
