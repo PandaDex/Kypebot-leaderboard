4add1e0d3a0ebbea8c94735646e8b8d8b1502fc7
Kacper Wochniak <kacperwochniak22@gmail.com>
2023-07-04 01:41:36 +0200

    1.0.2

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   useEffect(() => {
     const fetchLeaderboardData = () => {
-      fetch(`https://api.kypebot.xyz/leaderboard-data`)
+      fetch(`https://api.kypebot.xyz:2135/leaderboard-data`)
         .then(response => response.json())
         .then(data => setLeaderboardData(data))
         .catch(error => console.error(error));
@@ -19,28 +19,30 @@ function App() {
   }, []);
 
   return (
-    <div className="leaderboard">
+    <div className="leaderboard scrollbar">
       <div className="top">
         <div>Miejsce</div>
         <div className="header">Nick</div>
         <div>Jagerki</div>
       </div>
       <div className="body">
-      {leaderboardData.map((row, index) => (
-        <div
-          className={`leaderboard-row ${index === 0 ? 'first-place' : index === 1 ? 'second-place' : index === 2 ? 'third-place' : ''}`}
-          key={index}
-        >
-          <div className="leaderboard-rank">{index + 1}</div>
-          <div className="leaderboard-name">{row.username}</div>
-          <div className="leaderboard-points">{row.points.toLocaleString()}</div>
+        {leaderboardData.map((row, index) => (
+          <div
+            className={`leaderboard-row ${index === 0 ? 'first-place' : index === 1 ? 'second-place' : index === 2 ? 'third-place' : ''}`}
+            key={index}
+          >
+            <div className="leaderboard-rank">{index + 1}</div>
+            <div className="leaderboard-name">
+              {row.username}
+              {row.discord_id !== null && <img title="Połączono konto discord" draggable='false' src="discord.svg" alt="Discord Logo" className='Discord' />}
+            </div>
+            <div className="leaderboard-points">{row.points.toLocaleString()}</div>
+          </div>
+        ))}
+        <div>
+          <p className="footer">Powered by <a href='https://kypebot.xyz'>Kype</a></p>
         </div>
-      ))}
-      <div>
-        <p className='footer'>Made with ❤️ by DEX</p>
       </div>
-      </div>
-    
     </div>
   );
 }
